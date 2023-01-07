b893b7e85ebf916eeed95907005874a5c3d861d4
Manahan Lubis <72923118+manahan01@users.noreply.github.com>
2023-01-07 21:31:07 +0700

    Gameboard-Connect-4 React

---
+++ src/Components/GameCircle.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const onClick = (ev,id) => {
+    debugger;
+    alert('onClick'+ id);
+}
+
+const GameCircle = ({id, color, children}) => {
+    const style ={
+        backgroundColor: color,
+         width:100,
+          height:100,
+           margin:10,
+            borderRadius:'50%'
+    }
+    return (
+        <div style={style} onClick={(ev)=>onClick(ev,id)}>
+           {children}
+        </div>
+    )
+}
+
+export default GameCircle;
